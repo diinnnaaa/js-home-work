1c6aa39058765230d6c9af8f5831376b08e492ea
diinnnaaa <149644597+diinnnaaa@users.noreply.github.com>
2023-12-12 08:27:05 +0600

    js home worke 12.12

---
+++ JS/11.12-variables/index.js
@@ -0,0 +1,93 @@
+// console.log('Hello, World!')
+// console.log('hello,Dina')
+// console.log('1+1-2%1')
+// console.log(245-857)
+// let country = `kakashka`
+// let age = 45
+// let sabaka = `dog`
+// console.log ('country') 
+// console.log ('age')
+// console.log (country, age, sabaka )
+// // атауы мәні типі
+// // константе(const) дегендердын маны озгермейды. Например  число пи
+// // const мәні озгермейтін айнымалы
+// // АЙНЫМАЛЫНЫН МАНЫН ОЗГЕРТТЫК
+// country = `usa`
+// age = 18
+// sabaka = `died`
+// console.log(country, age, sabaka)
+// const p = 3.14
+// console.log(p)
+// const gender = `girl` 
+// console.log (gender)
+// console.log(`my name is ${sabaka}`)
+// // imenno osy ` ` zat bolu karek
+// let a = 5
+// let b = 6
+// console.log (`${a}+${b}= ${a+b}` )
+// console.log (`(${5}*${b})+(${b}/${a})=${a*b+b/a}`)
+
+// let al = 23
+// let ba = 67
+// let ca = 897
+// console.log (`${al}*${ba}+${ca}= ${al*ba+ca}`)
+
+
+// let name = `Дина`
+// let agel = 19
+// let city = `Алматыда`
+// console.log (`Менің атым ${name}. Мен ${agel} жастамын. Мен  ${city} тұрамын`)
+
+// let la = 786
+// let sa = 456
+// let ko = 348
+// console.log (`Менің атым ${name}. Мен ${la}*${sa}+${ko} есебын шагара алам. Жауабы: ${la*sa+ko}`)
+
+// УЙ ЖУМЫСЫ
+
+let favoriteColor = "сұр"
+let favoriteSong = "Shut up my moms calling"
+let favoriteMovie = "Магическая Битва"
+let favoriteAnimal = "Ит"
+let storyTitle = "Динанын кугынбайлыгы"
+
+let story = (`Менің өмірімде тыныштық. Менің ең сүйікті түсім ол ${favoriteColor}, өйткены менің ойымша ол маған жарасады және күшті көрінеді.
+"${favoriteSong}" менде әрдайым ойнайды (ол менын сүйікті өленім), особенно азанда сабаққа бара жатқанда және уйктайн деген кезде.
+${favoriteMovie} бұл менің кәзір көріп жүрген анимем, маған өте қатты онын ішінде Тодзи Фусигуро, енді онын формаларын айталмадым өөөөөөтттеее хот кейіпкер. 
+Менен сенын ен жақсы көретін үй жанураын кім дегенде мен әрдайым ${favoriteAnimal} деймін, үйткені олар адамға ең жақын дос. 
+${storyTitle} өмірімдегі жоғары мақсаттарды бағдарламалармен толтыруды жалғастыруды таңдауды ұмытпаймын.`)
+
+console.log(story)
+
+let hasCoffee = true
+if (hasCoffee) {
+    console.log("I have coffee. COOL!")
+} else {
+    console.log("No coffee,No love, FASHION FASHION F F F F FASHION.")
+}
+
+
+let isWeekend = false
+if (isWeekend) {
+  console.log("Бүгін выходной день!")
+} else {
+  console.log("Бүгін пахатететін күн!")
+}
+
+
+// float task
+let gravity = 9.8
+let weight = 60 
+//формула: с = м * г
+let forceOfGravity = weight * gravity
+
+console.log(`Сила тяжести, действующая на человека массой ${weight} кг, равна ${forceOfGravity} N.`)
+
+
+let temperatureInCelsius = 25.5
+// formula: (Celsius * 9/5) + 32
+let temperatureInFahrenheit = (temperatureInCelsius * 9/5) + 32
+
+console.log(`The temperature in Celsius is ${temperatureInCelsius}°C.`)
+console.log(`Converted to Fahrenheit, it is ${temperatureInFahrenheit}°F.`)
+
